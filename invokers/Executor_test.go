9bef4d89da6b34613105dac28fa5d3b46e671d1c
pnhung177 <pnhung177@acegik.net>
2018-12-30 20:59:47 +0700

    Improve testing code

--- invokers/Executor_test.go
+++ invokers/Executor_test.go
@@ -12,21 +12,28 @@ func TestExecutor_invokeCommand_pipeInput_stdout(t *testing.T) {
 			CommandString: "grep hello",
 		},
 	})
-	outBytes, errBytes, _, err := e.RunOnRawData(nil, []byte("hello grep\ngoodbye grep"))
+	outBytes, errBytes, state, err := e.RunOnRawData(nil, []byte("hello grep\ngoodbye grep"))
 	fmt.Printf("Stdout: [%s]\n", string(outBytes))
 	fmt.Printf("Stderr: [%s]\n", string(errBytes))
 	assert.Nil(t, err)
 	assert.Equal(t, outBytes, []byte("hello grep\n"))
 	assert.Equal(t, errBytes, []byte{})
+	assert.NotNil(t, state)
+	assert.True(t, state.Duration.Seconds() > 0)
 }
 
 func TestExecutor_invokeCommand_paramIn_stdout(t *testing.T) {
 	e, _ := NewExecutor(&ExecutorOptions{
 		Command: CommandDescriptor{
-			CommandString: "ps",
+			CommandString: "pwd",
 		},
 	})
-	out, err, _, _ := e.RunOnRawData(nil, nil)
-	fmt.Printf("Stdout: [%s]\n", string(out))
-	fmt.Printf("Stderr: [%s]\n", string(err))
+	outBytes, errBytes, state, err := e.RunOnRawData(nil, nil)
+	fmt.Printf("Stdout: [%s]\n", string(outBytes))
+	fmt.Printf("Stderr: [%s]\n", string(errBytes))
+	assert.Nil(t, err)
+	assert.True(t, len(outBytes) > 0)
+	assert.True(t, len(errBytes) == 0)
+	assert.NotNil(t, state)
+	assert.True(t, state.Duration.Seconds() > 0)
 }
