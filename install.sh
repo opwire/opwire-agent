72ce717c46e90a97eebe452440c9ced8af7b8818
pnhung177 <pnhung177@acegik.net>
2019-03-21 06:41:41 +0700

    Add usage() procedure

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
+
+usage() {
+  echo "Usage: curl https://opwire.org/opwire-agent/install.sh | sudo bash" 1>&2;
+  exit 1;
+}
+
+#check for help flag
+if [ -n "$1" ] && [ "$1" == "help" ]; then
+    usage
+fi
+
