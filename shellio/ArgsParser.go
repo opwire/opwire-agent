a709b2dc482f4ed010297e8bda2c3705aff4044c
pnhung177 <pnhung177@acegik.net>
2019-04-01 15:32:42 +0700

    Clear host, port default values from arguments

--- shellio/ArgsParser.go
+++ shellio/ArgsParser.go
@@ -8,8 +8,8 @@ import (
 )
 
 type AgentCmdArgs struct {
-	Host *string `short:"h" long:"host" description:"Agent server host" default:"0.0.0.0"`
-	Port *uint `short:"p" long:"port" description:"Agent server port" default:"17779"`
+	Host *string `short:"h" long:"host" description:"Agent http server host"`
+	Port *uint `short:"p" long:"port" description:"Agent http server port"`
 	DirectCommand *string `short:"d" long:"default-command" description:"The command string that will be executed directly"`
 	ConfigPath *string `short:"c" long:"config" description:"Explicit configuration file"`
 	StaticPath []string `short:"s" long:"static-path" description:"Path of static web resources"`
