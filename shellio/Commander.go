3de2f293dcde7e5fba5da52f4cb11fdf74737565
pnhung177 <pnhung177@acegik.net>
2019-04-09 10:29:56 +0700

    Refactoring cli package

--- shellio/Commander.go
+++ shellio/Commander.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	clp "github.com/urfave/cli"
 	"github.com/opwire/opwire-agent/services"
+	"github.com/opwire/opwire-agent/utils"
 )
 
 type AgentCommander struct {
@@ -78,4 +79,57 @@ func (c *AgentCommander) Run() error {
 		return fmt.Errorf("Agent commander has not initialized properly")
 	}
 	return c.app.Run(os.Args)
+}
+
+type AgentManifest interface {
+	GetRevision() string
+	GetVersion() string
+	String() (string, bool)
+}
+
+type AgentCmdFlags struct {
+	ConfigPath string
+	Host string
+	Port uint
+	DirectCommand string
+	StaticPath []string
+	manifest AgentManifest
+}
+
+func (a *AgentCmdFlags) GetConfigPath() string {
+	return a.ConfigPath
+}
+
+func (a *AgentCmdFlags) GetDirectCommand() string {
+	return a.DirectCommand
+}
+
+func (a *AgentCmdFlags) GetHost() string {
+	return a.Host
+}
+
+func (a *AgentCmdFlags) GetPort() uint {
+	return a.Port
+}
+
+func (a *AgentCmdFlags) GetStaticPath() map[string]string {
+	return utils.ParseDirMappings(a.StaticPath)
+}
+
+func (a *AgentCmdFlags) SuppressAutoStart() bool {
+	return false
+}
+
+func (a *AgentCmdFlags) GetRevision() string {
+	if a.manifest == nil {
+		return ""
+	}
+	return a.manifest.GetRevision()
+}
+
+func (a *AgentCmdFlags) GetVersion() string {
+	if a.manifest == nil {
+		return ""
+	}
+	return a.manifest.GetVersion()
 }
