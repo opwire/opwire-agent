7a46c17efa25df24f805b1b6922e6b98dc5b1b69
pnhung177 <pnhung177@acegik.net>
2019-04-16 15:12:50 +0700

    Wrap the zap logger

--- go.mod
+++ go.mod
@@ -14,6 +14,9 @@ require (
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.1.0
+	go.uber.org/atomic v1.3.2 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.9.1
 	golang.org/x/sync v0.0.0-20190227155943-e225da77a7e6
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
---
+++ logging/Logger.go
@@ -0,0 +1,94 @@
+package logging
+
+import (
+	"encoding/json"
+	"fmt"
+	"go.uber.org/zap"
+)
+
+type Field = zap.Field
+var Any = zap.Any
+var Array = zap.Array
+var Binary = zap.Binary
+var Bool = zap.Bool
+var Bools = zap.Bools
+var ByteString = zap.ByteString
+var ByteStrings = zap.ByteStrings
+var String = zap.String
+
+type LogLevel int
+
+const (
+	_ LogLevel = iota
+	DEBUG
+	INFO
+	WARN
+	ERROR
+	PANIC
+	FATAL
+)
+
+type Logger struct {
+	zapLogger *zap.Logger
+}
+
+var defaultConfig string = `{
+	"level": "debug",
+	"encoding": "console",
+	"outputPaths": ["stdout"],
+	"errorOutputPaths": ["stderr"],
+	"encoderConfig": {
+		"messageKey": "message",
+		"levelKey": "level",
+		"levelEncoder": "capital",
+		"timeKey": "logtime",
+		"timeEncoder": "ISO8601"
+	}
+}`
+
+func NewLogger() (*Logger, error) {
+	var cfg zap.Config
+	if err := json.Unmarshal([]byte(defaultConfig), &cfg); err != nil {
+		return nil, err
+	}
+
+	logger, err := cfg.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	l := new(Logger)
+	l.zapLogger = logger
+
+	return l, nil
+}
+
+func (l *Logger) assertReady() *Logger {
+	if l.zapLogger == nil {
+		panic(fmt.Errorf("Internal Logger must not be nil"))
+	}
+	return l
+}
+
+func (l *Logger) Log(level LogLevel, msg string, fields ...Field) {
+	l.assertReady()
+	switch(level) {
+	case DEBUG:
+		l.zapLogger.Debug(msg, fields...)
+	case INFO:
+		l.zapLogger.Info(msg, fields...)
+	case WARN:
+		l.zapLogger.Warn(msg, fields...)
+	case ERROR:
+		l.zapLogger.Error(msg, fields...)
+	case PANIC:
+		l.zapLogger.Panic(msg, fields...)
+	default:
+		l.zapLogger.Fatal(msg, fields...)
+	}
+}
+
+func (l *Logger) Sync() error {
+	l.assertReady()
+	return l.zapLogger.Sync()
+}
